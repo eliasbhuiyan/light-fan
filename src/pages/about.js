668666887107b460f4ce75be7e56f0d61e836914
bhuiyan21 <eliasbhuiyan21@gmail.com>
2023-05-27 20:10:37 +0600

    first commit

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,15 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
-const IndexPage = () => {
+const AboutPage = () => {
   return (
     <main>
       <h1>Welcome to my Gatsby site!</h1>
       <p>I'm making this by following the Gatsby Tutorial.</p>
-      <Link to='/about'>About</Link>
+      <Link to='/'>Home</Link>
     </main>
   )
 }
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <title>About Page</title>
 
-export default IndexPage
+export default AboutPage
