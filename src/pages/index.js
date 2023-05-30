846a6b12f85adffeeb1d813ca995c5b78624a403
bhuiyan21 <eliasbhuiyan21@gmail.com>
2023-05-30 13:51:16 +0600

    slid

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,25 +5,40 @@ import { StaticImage } from "gatsby-plugin-image"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import Seo from '../components/seo'
 import { graphql } from "gatsby"
+import Slider from "react-slick";
+
 const IndexPage = ({data}) => {
-  const image = getImage(data.allFile.nodes)
-  console.log(data,'===========');
+  const image = data.allFile.nodes;
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
   return (
+  
     <>
     <Layout pageTitle="Home Page">
-     <section className=''>
-      <div className='container'>
-        {/* <StaticImage src="../images/bg.png" layout='fullWidth' alt="Logo" /> */}
-      </div>
-     </section>
      <section>
       <div>
         {
-          image.map((image, index)=>{
-            <GatsbyImage image={image.childImageSharp.gatsbyImageData.images.fallback.src} />
-
-          })
+          image.map((image, index)=>(
+            <GatsbyImage image={image.childImageSharp.gatsbyImageData} alt={index}/>
+          ))
         }
+        <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+        </Slider>
       </div>
      </section>
       
